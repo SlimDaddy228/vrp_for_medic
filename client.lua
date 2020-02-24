442eb27802f04779c8b091c53352dfcd1a1f8b9d
SlimDaddy228 <60612282+SlimDaddy228@users.noreply.github.com>
2020-02-24 23:52:15 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -39,6 +39,7 @@ local function lech(wheelchairObject)
 			TaskPlayAnim(PlayerPedId(), 'missfinale_c2leadinoutfin_c_int', '_leadin_loop2_lester', 8.0, 8.0, -1, 69, 1, false, false, false)
 		end
 			if IsControlJustPressed(0, 73) then
+Citizen.Wait(10)
 				DetachEntity(wheelchairObject, true, true)
 				ClearPedTasks(PlayerPedId())
 		end
