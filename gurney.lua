c4ffd01101905fa490696e814497b95479d1cc6a
Vinipux322 <hsn10@bk.ru>
2020-02-26 03:05:13 +0300

    Change fucking waits and fix (100fps+) bug..

--- gurney.lua
+++ gurney.lua
@@ -32,7 +32,7 @@ local function GoToBed(wheelchairObject)
 	LoadAnim("anim@gangops@morgue@table@")
 	AttachEntityToEntity(ped, wheelchairObject, 0, -0.06, 0.1, 1.3, 0.0, 0.0, 179.0, 0.0, false, false, false, false, 2, true)
 	while IsEntityAttachedToEntity(ped, wheelchairObject) do
-		Wait(10)
+		Wait(5)
 		if not IsEntityPlayingAnim(ped, 'anim@gangops@morgue@table@', 'ko_front', 3) then
 			TaskPlayAnim(ped, 'anim@gangops@morgue@table@', 'ko_front', 8.0, 8.0, -1, 69, 1, false, false, false)
 		end
@@ -49,7 +49,7 @@ local function Pickup(wheelchairObject)
 	LoadAnim("anim@mp_ferris_wheel")
 	AttachEntityToEntity(wheelchairObject, ped, ped, 0.0, 1.8, -0.40 , 180.0, 180.0, 180.0, 0.0, false, false, true, false, 2, true)
 	while IsEntityAttachedToEntity(wheelchairObject, ped) do
-		Wait(10)
+		Wait(5)
 		if not IsEntityPlayingAnim(ped, 'anim@mp_ferris_wheel', 'idle_a_player_one', 3) then
 			TaskPlayAnim(ped, 'anim@mp_ferris_wheel', 'idle_a_player_one', 8.0, 8.0, -1, 50, 0, false, false, false)
 		end
@@ -63,7 +63,7 @@ end
 
 Citizen.CreateThread(function()
 	while true do
-		Wait(10)
+		Wait(5)
 		local pedCoords = GetEntityCoords(PlayerPedId())
 		local closestObject = GetClosestObjectOfType(pedCoords, 3.0, GetHashKey("v_med_bed1"), false)
 		if DoesEntityExist(closestObject) then
--- wheelchair.lua
+++ wheelchair.lua
@@ -35,12 +35,11 @@ local function Sit(wheelchairObject)
 	LoadAnim("missfinale_c2leadinoutfin_c_int")
 	AttachEntityToEntity(ped, wheelchairObject, 0, 0, 0.0, 0.4, 0.0, 0.0, 180.0, 0.0, false, false, false, false, 2, true)
 	while IsEntityAttachedToEntity(ped, wheelchairObject) do
-		Wait(10)
+		Wait(5)
 		if not IsEntityPlayingAnim(ped, 'missfinale_c2leadinoutfin_c_int', '_leadin_loop2_lester', 3) then
 			TaskPlayAnim(ped, 'missfinale_c2leadinoutfin_c_int', '_leadin_loop2_lester', 8.0, 8.0, -1, 69, 1, false, false, false)
 		end
 		if IsControlJustPressed(0, 73) then
-			Wait(10)
 			DetachEntity(wheelchairObject, true, true)
 			ClearPedTasks(ped)
 		end
@@ -58,7 +57,6 @@ local function Pickup(wheelchairObject)
 			TaskPlayAnim(ped, 'anim@heists@box_carry@', 'idle', 8.0, 8.0, -1, 50, 0, false, false, false)
 		end
 		if IsControlJustPressed(0, 73) then
-			Wait(20)
 			DetachEntity(wheelchairObject, true, true)
 			ClearPedTasks(ped)
 		end
@@ -67,7 +65,7 @@ end
 
 CreateThread(function()
 	while true do
-		Wait(10)
+		Wait(5)
 		local pedCoords = GetEntityCoords(PlayerPedId())
 		local closestObject = GetClosestObjectOfType(pedCoords, 10.0, GetHashKey("prop_wheelchair_01"), false)
 		local wheelChairCoords = GetEntityCoords(closestObject)
